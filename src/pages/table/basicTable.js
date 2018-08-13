3b5871127a1b3053d37559c0b98ed4639be611d9
王亚东 <Cotton.Wang@gaiaworks.cn>
2018-08-13 15:27:40 +0800

    基础表格

--- src/pages/table/basicTable.js
+++ src/pages/table/basicTable.js
@@ -4,7 +4,9 @@ import axios from '../../axios/index';
 
 export default class BasicTable extends React.Component {
     state = {
-        dataSource2: []
+        dataSource2: [],
+        selectedRowKeys: '',
+        selectedItem: ''
     }
 
     request = () => {
@@ -17,6 +19,9 @@ export default class BasicTable extends React.Component {
             }
         }).then(res => {
             if(res.code === 0){
+                res.result.map((item,index) => {
+                    item.key = index
+                })
                 this.setState({
                     dataSource2: res.result
                 })
@@ -33,24 +38,36 @@ export default class BasicTable extends React.Component {
             {
                 id: '1',
                 userName: 'Jack',
-                state: '1',
+                state: 1,
                 address: '江苏省苏州市虎丘区'
             },
             {
                 id: '2',
                 userName: 'CoCo',
-                state: '1',
+                state: 0,
                 address: '江苏省苏州市虎丘区'
             },
             {
                 id: '3',
                 userName: 'Mick',
-                state: '1',
+                state: 1,
                 address: '江苏省苏州市虎丘区'
             }
         ]
+        dataSource.map((item, index) => {
+            item.key = index
+        })
         this.setState({ dataSource })
     }
+
+    onRowClick = (record, index) => {
+        let selectKey = [index];
+        this.setState({
+            selectedRowKeys: selectKey,
+            selectedItem: record
+        })
+    }
+
     render() {
         const columns = [
             {
@@ -63,13 +80,23 @@ export default class BasicTable extends React.Component {
             },
             {
                 title: '状态',
-                dataIndex: 'state'
+                dataIndex: 'state',
+                render(state){
+                    return state === 1 ? '骑行中' : '空闲中'
+                }
             },
             {
                 title: '地址',
                 dataIndex: 'address'
             }
         ]
+
+        const {selectedRowKeys} = this.state;
+        const rowSelection = {
+            type: 'radio',
+            selectedRowKeys
+        }
+
         return (
             <div>
                 <Card title="基础表格">
@@ -80,9 +107,25 @@ export default class BasicTable extends React.Component {
                         pagination={false}>
                     </Table>
                 </Card>
-                <Card title="动态数据渲染表格" style={{margin: '10px 0'}}>
+                <Card title="动态数据渲染表格-Mock" style={{margin: '10px 0'}}>
                     <Table
                         bordered
+                        columns={columns}
+                        dataSource={this.state.dataSource2}
+                        pagination={false}>
+                    </Table>
+                </Card>
+                <Card title="Mock-单选" style={{margin: '10px 0'}}>
+                    <Table
+                        bordered
+                        rowSelection={rowSelection}
+                        onRow={(record, index) => {
+                            return {
+                                onClick: () => {
+                                    this.onRowClick(record, index)
+                                }
+                            }
+                        }}
                         columns={columns}
                         dataSource={this.state.dataSource2}
                         pagination={false}>
